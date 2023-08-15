2982870173a41b69b1e2ca75a0f80211f104f6ba
vadwwxz <vadwwxz@gmail.com>
2023-08-15 16:28:48 +0300

    first commit

---
+++ build-logic/publish-library-plugin/src/main/kotlin/com/sliderzxc/maven/publish/annotation/PublishDsl.kt
@@ -0,0 +1,4 @@
+package com.sliderzxc.maven.publish.annotation
+
+@DslMarker
+annotation class PublishDsl
---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        google()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
+
+rootProject.name = "cinematica-sdk"
+
+include(":sdk")
+
+includeBuild("build-logic/publish-library-plugin")
